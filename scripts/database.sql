3ee992180f9d1df7a7ec6fedc2359d1d66a78270
Hugo Barbon <151880308+Hugob21@users.noreply.github.com>
2025-02-13 20:02:03 +0100

    Update database.sql
    prueba sql 2

--- scripts/database.sql
+++ scripts/database.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
+    edad NUMBER(3) NOT NULL UNIQUE
 );
 
-INSERT INTO usuarios (nombre, email) VALUES
-('Juan Pérez', 'juan@example.com'),
-('María López', 'maria@example.com');
+INSERT INTO usuarios (nombre, edad) VALUES
+('Juan Pérez', 25),
+('María López', 22);
